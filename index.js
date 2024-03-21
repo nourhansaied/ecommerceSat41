0f24032d1b94e95de6eda5e04480ee2fdc27f503
nourhan <nourhan.saeed@almtar.com>
2024-03-21 03:51:19 +0200

    intro

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { allRoutes } from './src/modules/routes.js';
 const app = express()
 const port = 3000;
 
+
 app.use(cors())
 app.use("/uploads",express.static("uploads"))
 app.use(express.json());
